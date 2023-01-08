f14b11742624b969084ccc0be0e933261f3174e6
Yibo <yibojiang00@gmail.com>
2023-01-07 17:14:21 -0800

    update jump animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,24 +9,29 @@ public class PlayerController : MonoBehaviour
 
     public Character character;
 
+    public int playerId;
+
     protected CharacterActions characterActions;
 
     // Start is called before the first frame update
     void Start()
     {
         characterActions = new CharacterActions();
-        
-        characterActions.Up.AddDefaultBinding( Key.UpArrow );
-        characterActions.Up.AddDefaultBinding( InputControlType.DPadUp );
 
-        characterActions.Down.AddDefaultBinding( Key.DownArrow );
-        characterActions.Down.AddDefaultBinding( InputControlType.DPadDown );
+        if (playerId == 0)
+        {
+            characterActions.Up.AddDefaultBinding( Key.UpArrow );
+            characterActions.Up.AddDefaultBinding( InputControlType.DPadUp );
 
-        characterActions.Jump.AddDefaultBinding( Key.Space );
-        characterActions.Jump.AddDefaultBinding( InputControlType.Action1 );
+            characterActions.Down.AddDefaultBinding( Key.DownArrow );
+            characterActions.Down.AddDefaultBinding( InputControlType.DPadDown );
+
+            characterActions.Jump.AddDefaultBinding( Key.Space );
+            characterActions.Jump.AddDefaultBinding( InputControlType.Action1 );
         
-        characterActions.Jump.AddDefaultBinding( Key.Space );
-        characterActions.Jump.AddDefaultBinding( InputControlType.Action2 );
+            characterActions.Jump.AddDefaultBinding( Key.Space );
+            characterActions.Jump.AddDefaultBinding( InputControlType.Action2 );
+        }
     }
 
     // Update is called once per frame
