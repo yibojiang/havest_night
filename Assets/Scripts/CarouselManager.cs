6134b50ac7632f9b02c483cd34da6be16cad45c9
LuTxin <tianxindew@gmail.com>
2023-01-08 13:42:51 -0800

    Added carousel manager.

---
+++ Assets/Scripts/CarouselManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+class CarouselManager : MonoBehaviour
+{
+    [SerializeField] private GameObject[] items;
+    private float itemWidth;
+    private int currentLeft;
+
+    public void Start()
+    {
+        itemWidth = (items[items.Length - 1].transform.position.x - items[0].transform.position.x) / items.Length;
+        currentLeft = 0;
+    }
+
+    public void Update()
+    {
+        //update background
+        if (Camera.main.WorldToViewportPoint(items[(currentLeft + 1)% items.Length].transform.position).x < 0f)
+        {
+            Vector3 newPosition = new Vector3(items[(currentLeft - 1 + items.Length) % items.Length].transform.position.x + itemWidth, 
+                items[currentLeft].transform.position.y, 
+                items[currentLeft].transform.position.z);
+            items[currentLeft].transform.position = newPosition;
+
+            currentLeft = (currentLeft + 1) % items.Length;
+        }
+    }
+}
+
